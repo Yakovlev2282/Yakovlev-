3259327f83e2d033e8581d59c1643c0c9b55ef69
Yakovlev2282 <deoptgqqr31>
2016-04-22 16:09:48 +0300

    Work on C++ Yakovlev
    Work on C++ Yakovlev

---
+++ glava 1.2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int age;
+    cout<<"How old are You?\n";
+    cin>>age;
+    cout<<"You are"<<age<<"years old\n";
+   system ("pause");
+    return 0;
+}
