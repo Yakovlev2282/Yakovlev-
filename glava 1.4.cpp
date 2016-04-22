a91f4381f1262dc7d63881c158aa630e7f6a0386
Yakovlev2282 <deoptgqqr31>
2016-04-22 16:13:48 +0300

    af
    af

---
+++ glava 1.4.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+    double v;
+    double t=1.2;
+    double g=9.8;
+    v=12.3;
+    double s=v*t-g*t*t/2;
+    cout<<"Height  level is"<<s<<"\n";
+    system ("pause");
+    return 0;
+}
---
+++ glava 1.8.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+  char x;
+  int n=68;
+  int z;
+  x=n;
+  z='D';
+  cout<<"x="<<x<<"\n";
+  cout<<"z="<<z<<"\n";
+   system ("pause");
+    return 0;
+}
